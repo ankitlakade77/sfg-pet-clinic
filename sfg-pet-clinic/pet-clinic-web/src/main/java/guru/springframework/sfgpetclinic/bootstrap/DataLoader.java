500b2ae34113e5ef5881a326ced7660c3f6d5d1d
Ankit Lakade <44236379+ankitlakade9763@users.noreply.github.com>
2022-10-01 18:29:20 +0530

    closing issue #14 Closes

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -49,7 +49,7 @@ public class DataLoader implements CommandLineRunner{
 		
 		vetService.save(vet2);
 		
-		System.out.println("Loaded Vets...");
+		System.out.println("Loaded Vets....");
 		
 	}
 	

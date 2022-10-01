ab937c656aaa89176ced59fff9272987cb6132ba
Ankit Lakade <44236379+ankitlakade9763@users.noreply.github.com>
2022-10-01 21:00:14 +0530

    List Owner page is Ready. Closes #16

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import guru.springframework.sfgpetclinic.service.OwnerService;
 
 @Controller
 public class OwnerController {
+	
+	private OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService){
+		this.ownerService=ownerService;
+	}
 
 	@RequestMapping({"/owners/index","/owners"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+
+		model.addAttribute("owners",ownerService.findAll());
 		return "owners/index";
 	}
 }

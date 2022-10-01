94825896fba70b2bb2c7ec22978e58a8ac90dcf3
Ankit Lakade <44236379+ankitlakade9763@users.noreply.github.com>
2022-10-01 21:08:54 +0530

    Vet list page completed. Closes #15

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.service.VetService;
 
 @Controller
 public class VetController {
+	
+	private VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService=vetService;
+	}
 
 	@RequestMapping({"/vets","/vets/index"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets",vetService.findAll());
 		return "vets/index";
 	}
 }

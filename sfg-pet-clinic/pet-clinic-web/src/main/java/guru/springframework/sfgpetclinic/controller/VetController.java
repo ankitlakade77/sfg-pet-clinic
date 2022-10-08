1f3a0d02a20bb19ebab1e0be359637b942b65f1d
Ankit Lakade <44236379+ankitlakade9763@users.noreply.github.com>
2022-10-08 21:11:21 +0530

    Fixed broken links. Closes #45

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -3,7 +3,6 @@ package guru.springframework.sfgpetclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import guru.springframework.sfgpetclinic.service.VetService;
 
 @Controller

2433f97f9eab33ebc1bfde7156d3e4b658bb1c88
Ankit Lakade <44236379+ankitlakade77@users.noreply.github.com>
2022-11-13 16:03:05 +0530

    corrected owner display page. Closed #62

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -2,7 +2,11 @@ package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import guru.springframework.sfgpetclinic.service.OwnerService;
 
 @Controller
@@ -25,4 +29,11 @@ public class OwnerController {
 	public String findOwners() {
 		return "notimplemented";
 	}
+	
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
 }
